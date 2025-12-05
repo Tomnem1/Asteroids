aa4e1264a955fd226e3c2af4e075e8919a424469
Tomnem1 <tominemcik@gmail.com>
2025-12-05 18:22:33 +0100

    window close button terminates the game loop

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+
+        time_between_tick = clock.tick(60)
+        dt = time_between_tick/1000
+        # print(dt)
+
+
+if __name__ == "__main__":
+    main()
