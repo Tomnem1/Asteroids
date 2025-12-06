f3904fc7f3487b9fcc473ae30d1f1c67be919fab
Tomnem1 <tominemcik@gmail.com>
2025-12-06 17:54:30 +0100

    asteroid splitting, deleting asteroids + shots while coliding

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS, ASTEROID_KINDS
+from logger import log_event
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        
+        if(self.radius <= ASTEROID_MIN_RADIUS):
+            return
+        else:
+            log_event("asteroid_split")
+
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+            
+            split_angle = random.uniform(20, 50)
+            velocity_split_1 = self.velocity.rotate(split_angle)
+            velocity_split_2 = self.velocity.rotate(-split_angle)
+
+            asteroid1.velocity = velocity_split_1 * 1.2
+            asteroid2.velocity = velocity_split_2 * 1.2
--- main.py
+++ main.py
@@ -44,6 +44,13 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+        for asteroid in asteroids:
+            for shot in shots:
+               if(asteroid.collides_with(shot)):
+                   log_event("asteroid_shot")
+                   shot.kill()
+                   asteroid.split() 
+
         for object in drawable:
             object.draw(screen)
 
