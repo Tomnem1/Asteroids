6a9c76c7ab167f431984131d10a3bb503702d320
Tomnem1 <tominemcik@gmail.com>
2025-12-06 11:48:53 +0100

    Draw a player model on the screen + Implemented player movement

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -18,11 +19,16 @@ def main():
                 return
 
         screen.fill("black")
+
+        player.draw(screen)
+        player.update(dt)
+
         pygame.display.flip()
 
         time_between_tick = clock.tick(60)
         dt = time_between_tick/1000
-        # print(dt)
+
+
 
 
 if __name__ == "__main__":
